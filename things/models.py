92410dd5cb855856c797dab380df01d778fabc48
Siqi Li <1114673087@qq.com>
2023-10-08 16:16:49 +0100

    complete the exrcise3

--- things/models.py
+++ things/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Thing(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.CharField(max_length=100)
-    quantity = models.IntegerField()
+    name = models.CharField(blank=False, max_length=30, unique=True)
+    description = models.CharField(blank=True, max_length=120, unique=False)
+    quantity = models.PositiveIntegerField(unique= False, max_length=100, blank=False)
 
